655867d2d2e09861720c03edda6811c67511310d
Maciej Banaszynski <maciej.banaszynski@icloud.com>
2024-11-10 00:48:13 +0100

    Initial Commit

---
+++ combined_swiftui_app/combined_swiftui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  combined_swiftui_appApp.swift
+//  combined_swiftui_app
+//
+//  Created by Maciej Banaszyński on 10/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct combined_swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
